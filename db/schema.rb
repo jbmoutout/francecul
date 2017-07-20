d137c741514f088021213e25cbdd0f7cb886bc15
jbmoutout <jbmoutout@gmail.com>
2017-07-20 14:21:26 +0200

    postgres migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170720093323) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "emissions", force: :cascade do |t|
     t.string   "title"
     t.string   "link"
