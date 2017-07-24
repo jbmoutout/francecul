2a01aa2452065694a31c0c1960284cee82d11f82
jbmoutout <jbmoutout@gmail.com>
2017-07-24 10:06:42 +0200

    fix module eager loading (heroku)

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Francecul
   class Application < Rails::Application
     config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
