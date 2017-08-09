a82e8159b91653c3d9bb31ca8963ef775252a9c7
Jean-Brice Moutout <jbmoutout@gmail.com>
2017-08-09 19:02:13 +0200

    another API tweak

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,11 @@ module Francecul
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => '*',
-    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-  }
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
